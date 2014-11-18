2fa60ea5a81bd35b79278cb3b8b5752a6d387235
Aymeric GERLIER <aymeric.gerlier@sightcall.com>
2014-11-18 18:56:02 +0100

    scp draft

--- LSlave.h
+++ LSlave.h
@@ -161,6 +161,12 @@ class LSlave {
   static void idle();
   static void printInfo();
 
+  void begin(VirtualScreen* screen,
+            uint8_t rs, uint8_t enable,
+            uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3,
+            uint8_t backlightPin);
+  void begin(VirtualScreen* screen, LiquidCrystal* lcd);
+
   // inline
   __attribute__((always_inline)) inline static void uncheck();
 
